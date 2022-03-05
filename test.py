a81c75a14789d63448e0a4b441c032ec91abcf54
kyoF <fujiki.kyosuke@gmail.com>
2022-03-05 11:02:24 +0900

    映画のタイトルを取得

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://hlo.tohotheater.jp/net/schedule/076/TNPI2000J01.do")
+# response = requests.get("https://hlo.tohotheater.jp/net/schedule/076/TNPI2000J01.do")
 # 画像付き
-# response = requests.get("https://eiga.com/theater/13/130201/3263/")
+response = requests.get("https://eiga.com/theater/13/130201/3263/")
 soup = BeautifulSoup(response.content, "html.parser")
-title = soup.find("title").get_text()
-print(title)
+
+content_container_info = soup.find_all("div", class_="content-container")
+eiga_info = content_container_info[1].find_all("section")
+
+for eiga in eiga_info:
+    title = eiga.find("h2", class_="title-xlarge margin-top20").find("a").get_text()
+    
