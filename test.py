424e72a0e0982f7d7d2a38353ea2304f4c333dbf
kyoF <fujiki.kyosuke@gmail.com>
2022-03-06 23:19:23 +0900

    コメントの追加

--- test.py
+++ test.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup
 import datetime
 
 sinjuku_toho_theater = 'https://eiga.com/theater/13/130201/3263/'
-toho_detail_url = 'https://hlo.tohotheater.jp/net/movie/TNPI3060J01.do?sakuhin_cd='
+toho_reservation_url = 'https://hlo.tohotheater.jp/net/movie/TNPI3060J01.do?sakuhin_cd='
 
 response = requests.get(sinjuku_toho_theater)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 content_container_info = soup.find_all('div', class_='content-container')
+# 映画情報部分のみを取得
 eiga_info = content_container_info[1].find_all('section')
 
 # today = str(datetime.date.today())
@@ -22,15 +23,21 @@ today_time_schedule = []
 loop_index = 0
 
 for eiga in eiga_info:
+    # 今日放映する作品を取得
     tmp_schedule = eiga.find('div', class_='movie-schedule').find('td', attrs={'data-date':today.replace('-', '')})
+
     if tmp_schedule:
         today_time_schedule.append([])
+        
         today_schedule_info_list = tmp_schedule.find_all(['span', 'a'])
+        # 初めの要素は曜日情報なので削除
         today_schedule_info_list.pop(0)
         for time in today_schedule_info_list:
-            today_time_schedule[loop_index] = { 'schedule_time':time.get_text(), 'reservation_url':'' }
+            # 時間とそれに対応する席予約のURLを辞書型で取得
             toho_seat_url = time['href']
-            today_time_schedule[loop_index]['reservation_url'] = toho_seat_url
+            today_time_schedule[loop_index] = { 'schedule_time':time.get_text(), 'reservation_url':toho_seat_url }
+            
+            # 東宝のURLから作品コードを取得
             if len(sakuhin_code) != loop_index+1:
                 first_target_str = 'sakuhin_cd='
                 second_target_str = '&screen_cd='
@@ -39,7 +46,9 @@ for eiga in eiga_info:
                 tmp_sakuhin_code = toho_seat_url[first_idx+len(first_target_str):second_idx]
                 sakuhin_code.append(tmp_sakuhin_code)
 
+        # 作品タイトルを取得
         title.append(eiga.find('h2', class_='title-xlarge margin-top20').find('a').get_text())
+        # 作品の画像を取得
         # image_url.append(eiga.find('div', class_='movie-image').find('img', attrs={'alt':title[loop_index]})['src'])
         loop_index += 1
 
@@ -48,5 +57,5 @@ for i in range(len(title)):
     # print(f'image : {image_url[i]}')
     # print(f'image : {type(image_url[i])}')
     print(f'schedule : {today_time_schedule[i]}')
-    print(f'reservation : {toho_detail_url}{sakuhin_code[i]}')
+    print(f'reservation : {toho_reservation_url}{sakuhin_code[i]}')
     print('------------------------------')
