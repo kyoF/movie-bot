2b3f6eb04ba3446af82d2f556eed5b5923505589
kyoF <fujiki.kyosuke@gmail.com>
2022-03-06 23:01:53 +0900

    必要な情報を取得して出力するところまで完了（エラーもなし）（次の日の日付で処理実装）

--- test.py
+++ test.py
@@ -13,15 +13,13 @@ content_container_info = soup.find_all('div', class_='content-container')
 eiga_info = content_container_info[1].find_all('section')
 
 # today = str(datetime.date.today())
-today = '2022-03-07'
+today = '2022-03-07' # テスト用
 
 title = []
 # image_url = []
 today_time_schedule = []
 detail_url = []
 loop_index = 0
-# seat_url = []
-# seat_url_loop_index = 0
 sakuhin_code = []
 
 for eiga in eiga_info:
@@ -31,28 +29,20 @@ for eiga in eiga_info:
         today_schedule_info_list = tmp_schedule.find_all(['span', 'a'])
         today_schedule_info_list.pop(0)
         for time in today_schedule_info_list:
-            today_time_schedule[loop_index].append(time.get_text())
+            today_time_schedule[loop_index] = { 'schedule_time':time.get_text(), 'reservation_url':'' }
             if '~' in time.get_text() or ':' in time.get_text():
-                # seat_url.append([])
-                # tmp_time = time.get_text()[:4]
-                # unix_datetime = datetime.datetime.strptime(f'{today} {tmp_time}')
-                # unix_datetime = datetime.datetime.strptime(f'{today} {time.get_text()}')
-                # seat_url[seat_url_loop_index].append(time['href'])
-                seat_url = time['href']
-                today_time_schedule[loop_index].append(seat_url)
+                toho_seat_url = time['href']
+                today_time_schedule[loop_index]['reservation_url'] = toho_seat_url
                 if len(sakuhin_code) != loop_index+1:
-                    target_str = 'sakuhin_cd='
-                    ts = '&screen_cd='
-                    idx = seat_url.find(target_str)
-                    iiddxx = seat_url.find(ts)
-                    r = seat_url[idx+len(target_str):iiddxx]
-                    sakuhin_code.append(r)
+                    first_target_str = 'sakuhin_cd='
+                    second_target_str = '&screen_cd='
+                    first_idx = toho_seat_url.find(first_target_str)
+                    second_idx = toho_seat_url.find(second_target_str)
+                    tmp_sakuhin_code = toho_seat_url[first_idx+len(first_target_str):second_idx]
+                    sakuhin_code.append(tmp_sakuhin_code)
 
         title.append(eiga.find('h2', class_='title-xlarge margin-top20').find('a').get_text())
         # image_url.append(eiga.find('div', class_='movie-image').find('img', attrs={'alt':title[loop_index]})['src'])
-        seat_url = eiga.find('a', )
-        # relative_path = eiga.find('a', class_='btn inline-block')['href']
-        # detail_url.append(f'{main_url_path}{relative_path}')
         loop_index += 1
 
 for i in range(len(title)):
@@ -60,6 +50,5 @@ for i in range(len(title)):
     # print(f'image : {image_url[i]}')
     # print(f'image : {type(image_url[i])}')
     print(f'schedule : {today_time_schedule[i]}')
-    print(f'{toho_detail_url}{sakuhin_code[i]}')
-    # print(f'url : {detail_url[i]}')
+    print(f'reservation : {toho_detail_url}{sakuhin_code[i]}')
     print('------------------------------')
