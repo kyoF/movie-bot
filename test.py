9a1eb955cbd0cf80046f2600812683e336e15239
kyoF <fujiki.kyosuke@gmail.com>
2022-03-06 21:22:40 +0900

    imageURLをコメントアウトして必要な情報の取得処理実装を完了

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ today = datetime.date.today()
 day_info = str(today).replace('-', '')
 
 title = []
-image_url = []
+# image_url = []
 today_time_schedule = []
 detail_url = []
 loop_index = 0
@@ -29,14 +29,15 @@ for eiga in eiga_info:
             today_time_schedule[loop_index].append(time.get_text())
 
         title.append(eiga.find('h2', class_='title-xlarge margin-top20').find('a').get_text())
-        image_url.append(eiga.find('div', class_='movie-image').find('img', attrs={'src'}))
-        relative_path = eiga.find('a', attrs={'href'})
+        # image_url.append(eiga.find('div', class_='movie-image').find('img', attrs={'alt':title[loop_index]})['src'])
+        relative_path = eiga.find('a', class_='btn inline-block')['href']
         detail_url.append(f'{main_url_path}{relative_path}')
         loop_index += 1
 
 for i in range(len(title)):
     print(f'title : {title[i]}')
-    print(f'image : {image_url[i]}')
+    # print(f'image : {image_url[i]}')
+    # print(f'image : {type(image_url[i])}')
     print(f'schedule : {today_time_schedule[i]}')
     print(f'url : {detail_url[i]}')
     print('------------------------------')
