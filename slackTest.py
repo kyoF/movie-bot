13b504bd25a8dd9204cc49535bbad56494c1bb6c
kyoF <fujiki.kyosuke@gmail.com>
2022-03-13 21:59:53 +0900

    gitignoreでslackのテスト用ファイルを無視するように修正

--- slackTest.py
+++
@@ -1,8 +0,0 @@
-import slackweb
-import json
-
-json_open = open('slack_info.json', 'r')
-json_load = json.load(json_open)
-
-slack = slackweb.Slack(url=json_load["incoming_webhook_url"])
-slack.notify(text="pythonからslackへ通知テスト")
