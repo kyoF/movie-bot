9ffeacd29724351d1400fdf56d4482af3bbca6d1
kyoF <fujiki.kyosuke@gmail.com>
2022-03-16 21:38:13 +0900

    jsでスクレイピングできる雛形の作成

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var payload = {
+let payload = {
   unfurl_links: false,
   username: "MOVIE BOT",
   icon_emoji: ":movie_camera:",
@@ -13,7 +13,21 @@ var payload = {
   ],
 };
 
+const pick_up_title_test = () => {
+  const targetUrl = "https://eiga.com/theater/13/130201/3263/";
+  var html = UrlFetchApp.fetch(targetUrl).getContentText("UTF-8");
+  var title = Parser.data(html).from("<title>").to("</title>").build();
+  return title;
+};
+
 function slack_notify() {
+  payload.blocks.push({
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: pick_up_title_test(),
+    },
+  });
   var options = {
     method: "POST",
     contentType: "application/json",
