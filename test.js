1fe4c1c870cdfdaf0a0b8684cb8a767f3569ad34
kyoF <83649546+kyoF@users.noreply.github.com>
2022-03-17 17:54:20 +0900

    slackに映画情報サイトのtitleを出力

--- test.js
+++ test.js
@@ -1,37 +1,50 @@
-let payload = {
-  unfurl_links: false,
-  username: "MOVIE BOT",
-  icon_emoji: ":movie_camera:",
-  blocks: [
-    {
-      type: "header",
-      text: {
-        type: "plain_text",
-        text: "映画情報テスト",
-      },
-    },
-  ],
-};
-
-const pick_up_title_test = () => {
+function movieBot() {
+  const incommingWebhookUrl = 'incomming webhookのURL';
   const targetUrl = "https://eiga.com/theater/13/130201/3263/";
-  var html = UrlFetchApp.fetch(targetUrl).getContentText("UTF-8");
-  var title = Parser.data(html).from("<title>").to("</title>").build();
-  return title;
+  const toho = "";
+  const payload = {
+    unfurl_links: false,
+    username: "MOVIE BOT",
+    icon_emoji: ":movie_camera:",
+    blocks: [
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "映画情報テスト",
+        },
+      },
+    ],
+  };
+  const html = UrlFetchApp.fetch(targetUrl).getContentText("UTF-8");
+  
+  // タイトルを取得
+  const title = pickUpTitleTest(html)
+  // titleをslackフォーマットのテキストに格納
+  payload.blocks.push(outputTextFormOfSlack(title))
+  // slackに通知
+  slackNotify(incommingWebhookUrl, payload);
+}
+
+const pickUpTitleTest = (html) => Parser.data(html).from("<title>").to("</title>").build();
+
+const outputTextFormOfSlack = (title) => {
+  return(
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: title,
+      }
+    }
+  )
 };
 
-function slack_notify() {
-  payload.blocks.push({
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: pick_up_title_test(),
-    },
-  });
-  var options = {
+const slackNotify = (incommingWebhookUrl, payload) => {
+  const options = {
     method: "POST",
     contentType: "application/json",
     payload: JSON.stringify(payload),
   };
-  UrlFetchApp.fetch(postUrl, options);
+  UrlFetchApp.fetch(incommingWebhookUrl, options);
 }
